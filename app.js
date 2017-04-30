6d7c1b51b36fd0c122a5db7868140aef08841163
Liam Donovan <liamdonovan245@gmail.com>
2017-04-30 17:10:27 -0400

    Express static file up

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var http =require("http");
+var http = require("http");
 
 var server = http.createServer(function (request, response) {
   
   response.end("Hi");
 });
 
-server.listen(3000);
+server.listen(8080);
--- index.js
+++ index.js
@@ -6,16 +6,34 @@ var app = express();
 
 var port = process.env.PORT || 8000;
 
+var public = __dirname + "/public/";
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 
+app.get('/', function(request, response) {
+    response.sendFile(path.join(public + "index.html"));
+})
+
+
+/*
 
 
 var server = http.createServer(function (request, response) {
     
     var date = new Date();
+    var year = date.getYear().toString();
+    var month = (Number(date.getMonth()) + 1).toString();
+    var day = date.getUTCDate().toString();
+    var unix = date.getTime().toString();
+    
     var urlObj = url.parse(request.url, true);
     var time = urlObj.query;
     var pathname = urlObj.pathname;
+   
+    
+    response.sendFile(path.join(public + "index.html"));
+    
     
     if (pathname === "/") {
         response.end("new home page");
@@ -26,13 +44,21 @@ var server = http.createServer(function (request, response) {
     }
     
     else if (pathname === '/date') {
-        response.end(date.toJSON());
+        response.end(month + date + year);
     }
     
     
     
-});
+    
+    
+    
+    
+    
+}); 
 
-server.listen(port);
+*/
+
+
+app.listen(port);
 
 console.log('Server running on port ' + port);
